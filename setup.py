b6f8500c467e36b1a7e0f4ebfbde01d04566365f
Yash Kabra <Yash.Kabra@avisbudget.com>
2025-01-13 13:58:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,24 +3,25 @@ from setuptools import setup, find_packages
 setup(
     name="xml-formatter-plus",
     version="0.1.0",
+    author="Yash Kabra",
+    author_email="yash11kabra@gmail.com",
+    description="A Python package that formats and beautifies XML strings with HTML entity handling",
+    url="https://github.com/yash11K/html-escape",
     packages=find_packages(),
-    install_requires=[
-        "argparse",
-    ],
-    author="YashKabra",
-    description="A Python package to format and beautify XML strings with HTML entity handling",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/YashKabra/decoder-package",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    py_modules=["xml_formatter"],
     entry_points={
         "console_scripts": [
             "xml-formatter=xml_formatter:main",
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.6",
 )
